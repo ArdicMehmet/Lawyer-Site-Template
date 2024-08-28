2e9c474873c97b9bd7e8c0bc8084ede3f63dcbd6
Mehmet Ardic <ardicmehmet96@gmail.com>
2024-08-28 11:26:43 +0300

    changed some

--- js/main.js
+++ js/main.js
@@ -16,15 +16,15 @@
     
     
     // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 90) {
-            $('.nav-bar').addClass('nav-sticky');
-            $('.carousel, .page-header').css("margin-top", "73px");
-        } else {
-            $('.nav-bar').removeClass('nav-sticky');
-            $('.carousel, .page-header').css("margin-top", "0");
-        }
-    });
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 90) {
+    //         $('.nav-bar').addClass('nav-sticky');
+    //         $('.carousel, .page-header').css("margin-top", "71px");
+    //     } else {
+    //         $('.nav-bar').removeClass('nav-sticky');
+    //         $('.carousel, .page-header').css("margin-top", "0");
+    //     }
+    // });
     
     
     // Dropdown on mouse hover
